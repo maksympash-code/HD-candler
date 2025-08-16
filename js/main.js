f86d89a5815374fdef9b2eff6b6d4fc29cc84af2
Maksym Pashchenko <maksympash@knu.ua>
2025-08-16 20:51:02 +0300

    Sections Feedback and Certificates done for all devices

--- js/main.js
+++ js/main.js
@@ -18,8 +18,18 @@ const swiperTwo = new Swiper('.certificates__slider', {
 loop: true,
 slidesPerView: 3,
 spaceBetween: 20,
-
 pagination: {
     el: '.swiper-pagination',
+},
+breakpoints: {
+        640: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        360: {
+            slidesPerView: 1,
+        }
 },
 });
